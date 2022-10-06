59d9d74c96615fe75a5807b6751451c2eea1630a
lewiih001 <lewismunyambu@gmail.com>
2022-10-07 01:32:36 +0300

    add App.js

--- src/App.js
+++ src/App.js
@@ -1,17 +1,22 @@
 
-import { BrowserRouter, Route } from 'react-router-dom';
-import './App.css';
-import Header from './components/Header';
+import "./App.css";
+import Header from "./components/Header";
+import { BrowserRouter, Route } from "react-router-dom";
+import Home from "./components/Home";
+import Cart from "./components/Cart";
 
 function App() {
   return (
     <BrowserRouter>
-  <Header/>
-  <div>
-    <Route>
-      
-    </Route>
-  </div>
+      <Header />
+      <div className="App">
+        <Route path="/" exact>
+          <Home />
+        </Route>
+        <Route path="/cart">
+          <Cart />
+        </Route>
+      </div>
     </BrowserRouter>
   );
 }
